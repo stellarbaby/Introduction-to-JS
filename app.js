e7fb1b11947d6f47b53b42f0f5eff2a6b81a4c4e
Stellar <stellarcryptos@gmail.com>
2024-08-11 19:18:23 +0100

    final

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 'use strict';
 
-
 console.log ("This is my first js file");
 
 
@@ -12,3 +11,15 @@ const x = 5;
 const y = 2;
 const z = x + y;
 console.log (z);
+
+
+
+const priceOne = 'Two hundred';
+const priceTwo = 'Three throusand'
+let totalAmount = priceOne + priceTwo
+console.log (totalAmount);
+
+let text1 = 'what a very ';
+let text2 = 'nice day';
+let text3 = text1 + text2;
+console.log (text3);
