06474288f61df0fbd2a81c04a6c8806b41ee16ac
Stellar <stellarcryptos@gmail.com>
2024-08-11 19:29:15 +0100

    final

--- app.js
+++ app.js
@@ -4,7 +4,14 @@ console.log ("This is my first js file");
 
 
 let names = 'Stellar Heritage Oluwatobiloba';
+const age = 25;
 console.log (names);
+console.log (age);
+
+const person = {
+    names : 'John Bidemi Awolowo' 
+};
+console.log(person);
 
 
 const x = 5;
@@ -13,13 +20,14 @@ const z = x + y;
 console.log (z);
 
 
-
-const priceOne = 'Two hundred';
-const priceTwo = 'Three throusand'
-let totalAmount = priceOne + priceTwo
+const priceOne = 'Two hundred ';
+const priceTwo = 'Three throusand';
+let totalAmount = priceOne + priceTwo;
 console.log (totalAmount);
 
 let text1 = 'what a very ';
 let text2 = 'nice day';
 let text3 = text1 + text2;
 console.log (text3);
+
+
