12599371a6a4d8e5bedfcb5753fff45dd577250c
Stellar <stellarcryptos@gmail.com>
2024-08-11 19:07:13 +0100

    new

---
+++ app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+
+console.log ("This is my first js file");
+
+
+let names = 'Stellar Heritage Oluwatobiloba';
+console.log (names);
+
+
+const x = 5;
+const y = 2;
+const z = x + y;
+console.log (z);
